98c75ab7d77b6c7e9531ab81ff21c6dbd45a02e6
S-n-o-w-W-h-i-t-e <131187328+S-n-o-w-W-h-i-t-e@users.noreply.github.com>
2024-07-27 15:06:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menu = document.querySelector("#dropdown");
     const menucontent = document.querySelector(".dropdown-content");
-    const category = document.querySelector(".hidden");
-    const content = document.querySelectorAll(".banana");
+    const categorybanana = document.querySelector("#categorybanana");
+    const categorysweet = document.querySelector("#categorysweet");
+    const categorykappa = document.querySelector("#categorykappa");
+    const categorymurukk = document.querySelector("#categorymurukk");
+    const categorykadalamavu = document.querySelector("#categorykadalamavu");
+    const categoryspecial = document.querySelector("#categoryspecial");
+    const categoryother = document.querySelector("#categoryother");
+
+    const banana = document.querySelectorAll(".banana");
+    const sweet = document.querySelectorAll(".sweet");
+    const kappa = document.querySelectorAll(".kappa");
+    const murukk = document.querySelectorAll(".murukk");
+    const kadalamavu = document.querySelectorAll(".kadalamavu");
+    const special = document.querySelectorAll(".special");
+    const other = document.querySelectorAll(".other");
 
     function menudropdown() {
     	event.stopPropagation();
@@ -14,22 +27,112 @@ document.addEventListener("DOMContentLoaded", function() {
     	menucontent.style.display = "none"
     }
 
-	function myFunction() {
+	function bananafunction() {
 		event.stopPropagation();
-	    if (content[0].style.display === "block") {
-	    	for (item of content) {
+	    if (banana[0].style.display === "block") {
+	    	for (item of banana) {
             	item.style.display = "none";
 	    	}
 	    }
          else {
-            for (item of content) {
+            for (item of banana) {
             	item.style.display = "block";
         	}
     	}
 	}
 
+	function sweetfunction() {
+		event.stopPropagation();
+	    if (sweet[0].style.display === "block") {
+	    	for (item of sweet) {
+            	item.style.display = "none";
+	    	}
+	    }
+         else {
+            for (item of sweet) {
+            	item.style.display = "block";
+        	}
+    	}
+	}
+
+	function kappafunction() {
+		event.stopPropagation();
+	    if (kappa[0].style.display === "block") {
+	    	for (item of kappa) {
+            	item.style.display = "none";
+	    	}
+	    }
+         else {
+            for (item of kappa) {
+            	item.style.display = "block";
+        	}
+    	}
+	}
+
+	function murukkfunction() {
+		event.stopPropagation();
+	    if (murukk[0].style.display === "block") {
+	    	for (item of murukk) {
+            	item.style.display = "none";
+	    	}
+	    }
+         else {
+            for (item of murukk) {
+            	item.style.display = "block";
+        	}
+    	}
+    }
+
+    function kadalamavufunction() {
+		event.stopPropagation();
+	    if (kadalamavu[0].style.display === "block") {
+	    	for (item of kadalamavu) {
+            	item.style.display = "none";
+	    	}
+	    }
+         else {
+            for (item of kadalamavu) {
+            	item.style.display = "block";
+        	}
+    	}
+    }
+
+    function specialfunction() {
+		event.stopPropagation();
+	    if (special[0].style.display === "block") {
+	    	for (item of special) {
+            	item.style.display = "none";
+	    	}
+	    }
+         else {
+            for (item of special) {
+            	item.style.display = "block";
+        	}
+    	}
+    }
+
+    function otherfunction() {
+		event.stopPropagation();
+	    if (other[0].style.display === "block") {
+	    	for (item of other) {
+            	item.style.display = "none";
+	    	}
+	    }
+         else {
+            for (item of other) {
+            	item.style.display = "block";
+        	}
+    	}
+    }
+
 	menu.addEventListener("click", menudropdown)
     document.addEventListener("click", dropdownhide)
-    category.addEventListener("click", myFunction);
+    categorybanana.addEventListener("click", bananafunction);
+    categorysweet.addEventListener("click", sweetfunction);
+    categorykappa.addEventListener("click", kappafunction);
+    categorymurukk.addEventListener("click", murukkfunction);
+    categorykadalamavu.addEventListener("click", kadalamavufunction);
+    categoryspecial.addEventListener("click", specialfunction);
+    categoryother.addEventListener("click", otherfunction);
 });
 
